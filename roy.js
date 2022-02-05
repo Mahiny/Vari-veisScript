7837f0e33c1a61885cf5e0767271d3874b570617
Mahiny <93958141+Mahiny@users.noreply.github.com>
2022-02-04 21:44:27 -0300

    Add files via upload

---
+++ roy.js
@@ -0,0 +1,32 @@
+<meta charset="UTF-8">
+
+<script>
+var idadeLinda = 12;
+var idadeGuaraciara = 15;
+var idadePérola = 13;
+
+    document.write( "A média da idade das minhas filhas é :" + (idadeLinda + idadeGuaraciara + idadePérola) / 3);
+    document.write("<br>");
+
+</script>
+
+<script>
+var idadeLinda = 12;
+var idadeGuaraciara = 15;
+var idadePérola = 13;
+var totalIdades = 40;
+
+    document.write( "A média da idade das minhas filhas é :" + totalIdades / 3);
+    document.write("<br>");
+</script>
+
+<meta charset="UTF-8">
+<script>
+
+    var idadeLinda = 12;
+    var idadeGuaraciara = 15;
+    var idadePerola = 13;
+
+    var totalIdades = idadeLinda + idadeGuaraciara + idadePerola;
+    document.write( totalIdades / 3);
+</script>
